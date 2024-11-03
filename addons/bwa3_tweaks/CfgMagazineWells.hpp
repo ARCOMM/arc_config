62be647673874a1fb0a1145ec252c537cefc4a4d
Drofseh <Drofseh@users.noreply.github.com>
2024-11-03 03:20:55 -0800

    create BWA3 Tweaks addon (#7)
    * create BWA3 Tweaks addon
    
    - add CBA magazine wells
    - set ace_overheating_closedBolt
    
    * Update requiredAddons

---
+++ addons/bwa3_tweaks/CfgMagazineWells.hpp
@@ -0,0 +1,45 @@
+class CfgMagazineWells {
+    class CBA_45ACP_USP {
+        BWA3_Magazines[] = {"BWA3_12Rnd_45ACP_P12"};
+    };
+
+    class CBA_338LM_RS9 {
+        BWA3_Magazines[] = {"BWA3_10Rnd_86x70_G29", "BWA3_10Rnd_86x70_G29_Tracer"};
+    };
+
+    class CBA_46x30_MP7 {
+        BWA3_Magazines[] = {"BWA3_40Rnd_46x30_MP7", "BWA3_40Rnd_46x30_MP7_SD", "BWA3_20Rnd_46x30_MP7", "BWA3_20Rnd_46x30_MP7_SD"};
+    };
+
+    class CBA_50BMG_M107 {
+        BWA3_Magazines[] = {"BWA3_10Rnd_127x99_G82", "BWA3_10Rnd_127x99_G82_Tracer", "BWA3_10Rnd_127x99_G82_Tracer_Dim", "BWA3_10Rnd_127x99_G82_SD", "BWA3_10Rnd_127x99_G82_AP", "BWA3_10Rnd_127x99_G82_AP_Tracer", "BWA3_10Rnd_127x99_G82_Raufoss", "BWA3_10Rnd_127x99_G82_Raufoss_Tracer", "BWA3_10Rnd_127x99_G82_Raufoss_Tracer_Dim"};
+    };
+
+    class CBA_556x45_G36 {
+        BWA3_Magazines[] = {"BWA3_30Rnd_556x45_G36", "BWA3_30Rnd_556x45_G36_Tracer", "BWA3_30Rnd_556x45_G36_Tracer_Dim", "BWA3_30Rnd_556x45_G36_SD", "BWA3_30Rnd_556x45_G36_AP"};
+    };
+
+    class CBA_556x45_MINIMI {
+        BWA3_Magazines[] = {"BWA3_200Rnd_556x45", "BWA3_200Rnd_556x45_Tracer"};
+    };
+
+    class CBA_556x45_STANAG {
+        BWA3_Magazines[] = {"BWA3_30Rnd_556x45_G36", "BWA3_30Rnd_556x45_G36_Tracer", "BWA3_30Rnd_556x45_G36_Tracer_Dim", "BWA3_30Rnd_556x45_G36_SD", "BWA3_30Rnd_556x45_G36_AP"};
+    };
+
+    class CBA_762x51_AR10 {
+        BWA3_Magazines[] = {"BWA3_20Rnd_762x51_G28", "BWA3_20Rnd_762x51_G28_Tracer", "BWA3_20Rnd_762x51_G28_Tracer_Dim", "BWA3_20Rnd_762x51_G28_SD", "BWA3_20Rnd_762x51_G28_AP", "BWA3_20Rnd_762x51_G28_LR", "BWA3_10Rnd_762x51_G28", "BWA3_10Rnd_762x51_G28_Tracer", "BWA3_10Rnd_762x51_G28_Tracer_Dim", "BWA3_10Rnd_762x51_G28_SD", "BWA3_10Rnd_762x51_G28_AP", "BWA3_10Rnd_762x51_G28_LR"};
+    };
+
+    class CBA_762x51_LINKS {
+        BWA3_Magazines[] = {"BWA3_120Rnd_762x51_soft", "BWA3_120Rnd_762x51_Tracer_soft", "BWA3_120Rnd_762x51", "BWA3_120Rnd_762x51_Tracer"};
+    };
+
+    class CBA_9x19_USP {
+        BWA3_Magazines[] = {"BWA3_15Rnd_9x19_P8"};
+    };
+
+    class CBA_Carl_Gustaf {
+        BWA3_Magazines[] = {"BWA3_CarlGustav_Illum", "BWA3_CarlGustav_HE", "BWA3_CarlGustav_HEAT", "BWA3_CarlGustav_HEDP", "BWA3_CarlGustav_Smoke"};
+    };
+};
---
+++ addons/bwa3_tweaks/config.cpp
@@ -0,0 +1,31 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = CSTRING(component);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "arc_cfg_main",
+            "bwa3_carlgustav",
+            "bwa3_g28",
+            "bwa3_g29",
+            "bwa3_g36",
+            "bwa3_g38",
+            "bwa3_g82",
+            "bwa3_mg3",
+            "bwa3_mg4",
+            "bwa3_mg5",
+            "bwa3_mp7",
+            "bwa3_p12",
+            "bwa3_p8",
+            "bwa3_weapons"
+        };
+        author = ARC_AUTHOR;
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgMagazineWells.hpp"
+#include "CfgWeapons.hpp"
