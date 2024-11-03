3aa62e2bfb4d9dae488ef5f36fc4ffd6ccf91e91
Drofseh <Drofseh@users.noreply.github.com>
2024-11-02 17:02:34 -0700

    more magwells

---
+++ addons/cwr3_tweaks/CfgMagazineWells.hpp
@@ -0,0 +1,134 @@
+class CfgMagazineWells {
+// cwr3_5rnd_cz550_m for cwr3_srifle_cz550 should be given a magwell, but the classes they derive from (CUP_5x_22_LR_17_HMR_M for CUP_srifle_CZ550) are a little confused about what they want to be and also don't have magwells.
+
+    class CBA_12g_1rnd {
+        CWR3_Magazines[] = {
+            "cwr3_1rnd_shell_kozlice_m",
+            "cwr3_1rnd_ball_kozlice_m"
+        };
+    };
+
+    class CBA_303B_LeeEn {
+        CWR3_Magazines[] = {
+            "cwr3_10rnd_enfield_m"
+        };
+    };
+
+    class CBA_32ACP_Vz61 {
+        CWR3_Magazines[] = {
+            "cwr3_10rnd_skorpion_m"
+        };
+    };
+
+    class CBA_45ACP_Grease {
+        CWR3_Magazines[] = {
+            "cwr3_30rnd_m3a1_m"
+        };
+    };
+
+    class CBA_556x45_MINIMI {
+        CWR3_Magazines[] = {
+            "cwr3_200rnd_m249_rt_m"
+        };
+    };
+
+    class CBA_762x25_TT {
+        CWR3_Magazines[] = {
+            "cwr3_8rnd_tokarev_m"
+        };
+    };
+
+    class CBA_762x51_FAL_L {
+        CWR3_Magazines[] = {
+            "cwr3_30rnd_762x51_bren_m"
+        };
+    };
+
+    class CBA_762x51_5rnds {
+        CWR3_Magazines[] = {
+            "cwr3_5rnd_m24_m"
+        };
+    };
+
+    class CBA_762x51_LINKS {
+        CWR3_Magazines[] = {
+            "cwr3_100rnd_m60_wt_m",
+            "cwr3_120rnd_mg3_wt_m"
+        };
+    };
+
+    class CBA_762x51_M14 {
+        CWR3_Magazines[] = {
+            "cwr3_20rnd_m14_m"
+        };
+    };
+
+    class CBA_762x54R_LINKS {
+        CWR3_Magazines[] = {
+            "cwr3_100rnd_pkm_gt_m"
+        };
+    };
+
+    class CBA_762x54R_SVD {
+        CWR3_Magazines[] = {
+            "cwr3_10rnd_svd_m"
+        };
+    };
+
+    class CBA_762x54R_Vz59_LINKS {
+        CWR3_Magazines[] = {
+            "cwr3_50rnd_uk59_m"
+        };
+    };
+
+    class CBA_9x19_PP19 {
+        CWR3_Magazines[] = {
+            "cwr3_64rnd_bizon_m"
+        };
+    };
+
+    class CBA_9x19_MP5 {
+        CWR3_Magazines[] = {
+            "cwr3_30rnd_mp5_m"
+        };
+    };
+
+    class CBA_9x19_Sterling {
+        CWR3_Magazines[] = {
+            "cwr3_30rnd_sterling_m"
+        };
+    };
+    /*
+    // this magwell does not actually exist at this time.
+    class CBA_M67 {
+        CWR3_Magazines[] = {
+            "cwr3_m67_rcl_heat_m"
+        };
+    };
+    */
+    /*
+    // this magwell does not actually exist at this time.
+    class CBA_M47_Dragon {
+        CWR3_Magazines[] = {
+            "cwr3_m47_m"
+        };
+    };
+    */
+    class CBA_Carl_Gustaf {
+        CWR3_Magazines[] = {
+            "cwr3_carlgustaf_heat_m",
+            "cwr3_carlgustaf_hedp_m"
+        };
+    };
+
+    class CBA_RPG7 {
+        CWR3_Magazines[] = {
+            "cwr3_pg7v_m",
+            "cwr3_pg7vm_m",
+            "cwr3_pg7vl_m",
+            "cwr3_pg7vr_m",
+            "cwr3_og7_m",
+            "cwr3_tbg7v_m"
+        };
+    };
+};
--- addons/cwr3_tweaks/CfgWeapons.hpp
+++ addons/cwr3_tweaks/CfgWeapons.hpp
@@ -1,14 +1,143 @@
 class CfgWeapons {
+    class CUP_arifle_FNFAL5060;
+    class cwr3_lmg_bren : CUP_arifle_FNFAL5060 {
+        magazineWell[] += {"CBA_762x51_FAL_L","CBA_762x51_FAL_XL"};
+    };
+
+    class CUP_hgun_TaurusTracker455;
+    class cwr3_hgun_revolver : CUP_hgun_TaurusTracker455 {
+        magazineWell[] += {"CBA_32ACP_Vz61"};
+    };
+
+    class CUP_hgun_SA61;
+    class cwr3_hgun_sa61 : CUP_hgun_SA61 {
+        magazineWell[] += {"CBA_32ACP_Vz61"};
+    };
+
+    class CUP_hgun_TT;
+    class cwr3_hgun_tokarev : CUP_hgun_TT {
+        magazineWell[] += {"CBA_762x25_TT"};
+    };
+
+    class CUP_lmg_M249_E1;
+    class cwr3_lmg_m249e1 : CUP_lmg_M249_E1 {
+        magazineWell[] += {"CUP_lmg_M249_E1"};
+    };
+
     class CUP_lmg_M60;
+    class cwr3_lmg_m60 : CUP_lmg_M60 {
+        magazineWell[] += {"CBA_762x51_LINKS"};
+    };
+
     class CUP_lmg_M60E4_norail;
-
-    class cwr3_lmg_m60: CUP_lmg_M60 {
-        magazineWell[] += {"CBA_762x51_LINKS "};
+    class cwr3_lmg_m60e4 : CUP_lmg_M60E4_norail {
+        magazineWell[] += {"CBA_762x51_LINKS"};
     };
-    class cwr3_lmg_m60e4: CUP_lmg_M60E4_norail {
-        magazineWell[] += {"CBA_762x51_LINKS "};
+    class cwr3_lmg_m60e3 : cwr3_lmg_m60e4 {
+        magazineWell[] += {"CBA_762x51_LINKS"};
     };
 
+    class CUP_lmg_MG3;
+    class cwr3_lmg_mg3 : CUP_lmg_MG3 {
+        magazineWell[] += {"CBA_762x51_LINKS"};
+    };
+
+    class CUP_lmg_PKMN;
+    class cwr3_lmg_pkm : CUP_lmg_PKMN {
+        magazineWell[] += {"CBA_762x54R_LINKS"};
+    };
+
+    class CUP_lmg_UK59;
+    class cwr3_lmg_uk59 : CUP_lmg_UK59 {
+        magazineWell[] += {"CBA_762x54R_Vz59_LINKS"};
+    };
+
+    class CUP_smg_bizon;
+    class cwr3_sgun_kozlice : CUP_sgun_CZ584 {
+        magazineWell[] += {"CBA_12g_1rnd"};
+    };
+
+    class CUP_smg_bizon;
+    class cwr3_smg_bizon : CUP_smg_bizon {
+        magazineWell[] += {"CBA_9x19_PP19"};
+    };
+    class cwr3_smg_bizon_sd : CUP_smg_bizon {
+        magazineWell[] += {"CBA_9x19_PP19"};
+    };
+
+    class CUP_smg_M3A1;
+    class cwr3_smg_m3a1 : CUP_smg_M3A1 {
+        magazineWell[] += {"CBA_45ACP_Grease"};
+    };
+
+    class CUP_smg_MP5A5;
+    class cwr3_smg_mp5a5 : CUP_smg_MP5A5 {
+        magazineWell[] += {"CBA_9x19_MP5"};
+    };
+    class cwr3_smg_sterling : CUP_smg_MP5A5 {
+        magazineWell[] += {"CBA_9x19_Sterling"};
+    };
+
+    class CUP_smg_MP5SD6;
+    class cwr3_smg_mp5sd6 : CUP_smg_MP5SD6 {
+        magazineWell[] += {"CBA_9x19_MP5"};
+    };
+
+    class CUP_smg_MP5SD6;
+    class cwr3_smg_sterling_sd : CUP_smg_MP5SD6 {
+        magazineWell[] += {"CBA_9x19_Sterling"};
+    };
+    /*
+    // cwr3_5rnd_cz550_m for cwr3_srifle_cz550 should be given a magwell, but the classes they derive from (CUP_5x_22_LR_17_HMR_M for CUP_srifle_CZ550) are a little confused about what they want to be and also don't have magwells.
+    class CUP_srifle_CZ550;
+    class cwr3_srifle_cz550 : CUP_srifle_CZ550 {
+        magazineWell[] += {"xxxxx"};
+    };
+    */
+    class CUP_srifle_LeeEnfield;
+    class cwr3_srifle_enfield : CUP_srifle_LeeEnfield {
+        magazineWell[] += {"CBA_303B_LeeEn"};
+    };
+
+    class CUP_srifle_M21_artel;
+    class cwr3_srifle_m21 : CUP_srifle_M21_artel {
+        magazineWell[] += {"CBA_762x51_M14"};
+    };
+
+    class CUP_srifle_M24_wdl;
+    class cwr3_srifle_m24 : CUP_srifle_M24_wdl {
+        magazineWell[] += {"CBA_762x51_1rnd", "CBA_762x51_2rnds", "CBA_762x51_3rnds", "CBA_762x51_4rnds", "CBA_762x51_5rnds"};
+    };
+
+    class CUP_srifle_SVD_pso;
+    class cwr3_srifle_svd : CUP_srifle_SVD_pso {
+        magazineWell[] += {"CBA_762x54R_SVD"};
+    };
+    /*
+    // There is no CBA magwell for this weapon at this time
+    class CUP_launch_MAAWS;
+    class cwr3_launch_m67_rcl : CUP_launch_MAAWS {
+        magazineWell[] += {"CBA_M67"};
+    };
+    */
+    /*
+    // There is no CBA magwell for this weapon at this time
+    class CUP_launch_M47;
+    class cwr3_launch_m47 : CUP_launch_M47 {
+        magazineWell[] += {"CBA_M47_Dragon"};
+    };
+    */
+    class CUP_launch_MAAWS;
+    class cwr3_launch_carlgustaf : CUP_launch_MAAWS {
+        magazineWell[] += {"CBA_Carl_Gustaf"};
+    };
+
+    class CUP_launch_RPG7V;
+    class cwr3_launch_rpg7v : CUP_launch_RPG7V {
+        magazineWell[] += {"CBA_RPG7"};
+    };
+
+    // Vests
 	class ItemCore;
 	class cwr3_b_vest_base: ItemCore
 	{
