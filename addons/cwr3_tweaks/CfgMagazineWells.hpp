399dee5062c1e1495a9ab068785c0695c776a456
Drofseh <Drofseh@users.noreply.github.com>
2024-11-02 22:31:17 -0700

    Add CBA magwells to CWR weapons

--- addons/cwr3_tweaks/CfgMagazineWells.hpp
+++ addons/cwr3_tweaks/CfgMagazineWells.hpp
@@ -1,134 +1,159 @@
 class CfgMagazineWells {
-// cwr3_5rnd_cz550_m for cwr3_srifle_cz550 should be given a magwell, but the classes they derive from (CUP_5x_22_LR_17_HMR_M for CUP_srifle_CZ550) are a little confused about what they want to be and also don't have magwells.
+// cwr3_5rnd_cz550_m for cwr3_srifle_cz550 should be given a magwell, but the classes they derive from (CUP_5x_22_LR_17_HMR_M for CUP_srifle_CZ550) are a little confused about what they want to be and also don't have CBA magwells.
 
     class CBA_12g_1rnd {
-        CWR3_Magazines[] = {
-            "cwr3_1rnd_shell_kozlice_m",
-            "cwr3_1rnd_ball_kozlice_m"
-        };
+        CWR3_Magazines[] = {"cwr3_1rnd_shell_kozlice_m","cwr3_1rnd_ball_kozlice_m"};
     };
 
     class CBA_303B_LeeEn {
-        CWR3_Magazines[] = {
-            "cwr3_10rnd_enfield_m"
-        };
+        CWR3_Magazines[] = {"cwr3_10rnd_enfield_m"};
     };
 
     class CBA_32ACP_Vz61 {
-        CWR3_Magazines[] = {
-            "cwr3_10rnd_skorpion_m"
-        };
+        CWR3_Magazines[] = {"cwr3_10rnd_skorpion_m"};
+    };
+
+    class CBA_45ACP_Clip_6rnds {
+        CWR3_Magazines[] = {"cwr3_6rnd_revolver_m"};
+    };
+
+    class CBA_45ACP_MAC10 {
+        CWR3_Magazines[] = {"cwr3_30rnd_mac10_m"};
+    };
+
+    class CBA_45ACP_1911 {
+        CWR3_Magazines[] = {"cwr3_7rnd_m1911_m"};
     };
 
     class CBA_45ACP_Grease {
-        CWR3_Magazines[] = {
-            "cwr3_30rnd_m3a1_m"
-        };
+        CWR3_Magazines[] = {"cwr3_30rnd_m3a1_m"};
+    };
+
+    class CBA_556x45_G36 {
+        CWR3_Magazines[] = {"cwr3_30rnd_g36_m"};
     };
 
     class CBA_556x45_MINIMI {
-        CWR3_Magazines[] = {
-            "cwr3_200rnd_m249_rt_m"
-        };
+        CWR3_Magazines[] = {"cwr3_200rnd_m249_rt_m"};
+    };
+
+    class CBA_556x45_STANAG {
+        CWR3_Magazines[] = {"cwr3_30rnd_m16_m","cwr3_20rnd_m16_m"};
+    };
+
+    class CBA_556x45_STEYR {
+        CWR3_Magazines[] = {"cwr3_30rnd_steyr_m"};
+    };
+
+    class CBA_545x39_AK {
+        CWR3_Magazines[] = {"cwr3_30rnd_ak74_m","cwr3_30rnd_ak74_plum_m","cwr3_45rnd_rpk74_gt_m"};
     };
 
     class CBA_762x25_TT {
-        CWR3_Magazines[] = {
-            "cwr3_8rnd_tokarev_m"
-        };
+        CWR3_Magazines[] = {"cwr3_8rnd_tokarev_m"};
     };
 
-    class CBA_762x51_FAL_L {
-        CWR3_Magazines[] = {
-            "cwr3_30rnd_762x51_bren_m"
-        };
+    class CBA_762x39_AK {
+        CWR3_Magazines[] = {"cwr3_30rnd_akm_m","cwr3_30rnd_ak47_m","cwr3_75rnd_rpk_gt_m"};
+    };
+
+    class CBA_762x39_VZ58 {
+        CWR3_Magazines[] = {"cwr3_30rnd_sa58_m"};
+    };
+
+    class CBA_762x39_SKS_Clip {
+        CWR3_Magazines[] = {"cwr3_10rnd_sks_m"};
     };
 
     class CBA_762x51_5rnds {
-        CWR3_Magazines[] = {
-            "cwr3_5rnd_m24_m"
-        };
+        CWR3_Magazines[] = {"cwr3_5rnd_m24_m"};
+    };
+
+    class CBA_762x51_FAL_L {
+        CWR3_Magazines[] = {"cwr3_30rnd_762x51_bren_m"};
+    };
+
+    class CBA_762x51_G3 {
+        CWR3_Magazines[] = {"cwr3_20rnd_g3_m"};
     };
 
     class CBA_762x51_LINKS {
-        CWR3_Magazines[] = {
-            "cwr3_100rnd_m60_wt_m",
-            "cwr3_120rnd_mg3_wt_m"
-        };
+        CWR3_Magazines[] = {"cwr3_100rnd_m60_wt_m","cwr3_120rnd_mg3_wt_m"};
     };
 
     class CBA_762x51_M14 {
-        CWR3_Magazines[] = {
-            "cwr3_20rnd_m14_m"
-        };
+        CWR3_Magazines[] = {"cwr3_20rnd_m14_m"};
     };
 
     class CBA_762x54R_LINKS {
-        CWR3_Magazines[] = {
-            "cwr3_100rnd_pkm_gt_m"
-        };
+        CWR3_Magazines[] = {"cwr3_100rnd_pkm_gt_m"};
     };
 
     class CBA_762x54R_SVD {
-        CWR3_Magazines[] = {
-            "cwr3_10rnd_svd_m"
-        };
+        CWR3_Magazines[] = {"cwr3_10rnd_svd_m"};
     };
 
     class CBA_762x54R_Vz59_LINKS {
-        CWR3_Magazines[] = {
-            "cwr3_50rnd_uk59_m"
-        };
+        CWR3_Magazines[] = {"cwr3_50rnd_uk59_m"};
+    };
+
+    class CBA_9x18_APS {
+        CWR3_Magazines[] = {"cwr3_20rnd_9x18_aps_m"};
+    };
+
+    class CBA_9x18_PM {
+        CWR3_Magazines[] = {"cwr3_8rnd_makarov_m"};
+    };
+
+    class CBA_9x19_CZ75_Full {
+        CWR3_Magazines[] = {"cwr3_16rnd_cz75_m"};
+    };
+
+    class CBA_9x19_Glock_Full {
+        CWR3_Magazines[] = {"cwr3_17rnd_glock17_m"};
     };
 
     class CBA_9x19_PP19 {
-        CWR3_Magazines[] = {
-            "cwr3_64rnd_bizon_m"
-        };
+        CWR3_Magazines[] = {"cwr3_64rnd_bizon_m"};
+    };
+
+    class CBA_9x19_M9 {
+        CWR3_Magazines[] = {"cwr3_15rnd_m9_m"};
     };
 
     class CBA_9x19_MP5 {
-        CWR3_Magazines[] = {
-            "cwr3_30rnd_mp5_m"
-        };
+        CWR3_Magazines[] = {"cwr3_30rnd_mp5_m"};
     };
 
     class CBA_9x19_Sterling {
-        CWR3_Magazines[] = {
-            "cwr3_30rnd_sterling_m"
-        };
+        CWR3_Magazines[] = {"cwr3_30rnd_sterling_m"};
     };
-    /*
-    // this magwell does not actually exist at this time.
+
+    class CBA_9x19_UZI {
+        CWR3_Magazines[] = {"cwr3_30rnd_uzi_m"};
+    };
+
+    class CBA_40mm_GP_6rnds {
+        CWR3_Magazines[] = {"cwr3_6rnd_6g30_m"};
+    };
+
+    class CBA_40mm_M203_12rnds {
+        CWR3_Magazines[] = {"cwr3_12rnd_mm1_m"};
+    };
+
     class CBA_M67 {
-        CWR3_Magazines[] = {
-            "cwr3_m67_rcl_heat_m"
-        };
+        CWR3_Magazines[] = {"cwr3_m67_rcl_heat_m"};
     };
-    */
-    /*
-    // this magwell does not actually exist at this time.
+
     class CBA_M47_Dragon {
-        CWR3_Magazines[] = {
-            "cwr3_m47_m"
-        };
+        CWR3_Magazines[] = {"cwr3_m47_m"};
     };
-    */
+
     class CBA_Carl_Gustaf {
-        CWR3_Magazines[] = {
-            "cwr3_carlgustaf_heat_m",
-            "cwr3_carlgustaf_hedp_m"
-        };
+        CWR3_Magazines[] = {"cwr3_carlgustaf_heat_m","cwr3_carlgustaf_hedp_m"};
     };
 
     class CBA_RPG7 {
-        CWR3_Magazines[] = {
-            "cwr3_pg7v_m",
-            "cwr3_pg7vm_m",
-            "cwr3_pg7vl_m",
-            "cwr3_pg7vr_m",
-            "cwr3_og7_m",
-            "cwr3_tbg7v_m"
-        };
+        CWR3_Magazines[] = {"cwr3_pg7v_m","cwr3_pg7vm_m","cwr3_pg7vl_m","cwr3_pg7vr_m","cwr3_og7_m","cwr3_tbg7v_m"};
     };
 };
