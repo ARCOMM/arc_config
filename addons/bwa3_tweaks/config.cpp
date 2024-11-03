1a0783cba16b96f65d964e53a7b14b38dbf46021
Drofseh <Drofseh@users.noreply.github.com>
2024-11-03 00:05:47 -0700

    Update requiredAddons

--- addons/bwa3_tweaks/config.cpp
+++ addons/bwa3_tweaks/config.cpp
@@ -8,7 +8,19 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "arc_cfg_main",
-            "cwr3_soldiers_us",
+            "bwa3_carlgustav",
+            "bwa3_g28",
+            "bwa3_g29",
+            "bwa3_g36",
+            "bwa3_g38",
+            "bwa3_g82",
+            "bwa3_mg3",
+            "bwa3_mg4",
+            "bwa3_mg5",
+            "bwa3_mp7",
+            "bwa3_p12",
+            "bwa3_p8",
+            "bwa3_weapons"
         };
         author = ARC_AUTHOR;
         VERSION_CONFIG;
