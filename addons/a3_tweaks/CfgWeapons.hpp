7edaaf52a6bfe06fdd0f17486bf3165eb48f9576
tim4242 <puzzleddev@gmail.com>
2024-11-11 19:58:06 +0100

    Re-add NG5

--- addons/a3_tweaks/CfgWeapons.hpp
+++ addons/a3_tweaks/CfgWeapons.hpp
@@ -32,4 +32,44 @@ class CfgWeapons {
 
 	class Launcher_Base_F;
 	HIDE_CLASS(launch_RPG7_F,Launcher_Base_F);
+
+	class LMG_Zafir_F;
+	class GVAR(Negev_NG5): LMG_Zafir_F {
+		displayName = "Negev NG5";
+		descriptionShort = "Light Machine Gun<br />Caliber: 5.56x45 mm";
+
+		baseWeapon = QGVAR(Negev_NG5);
+
+		recoil = "recoil_lim";
+
+		magazineWell[] = {"CBA_556x45_MINIMI"};
+		magazines[] = {
+			"200Rnd_556x45_Box_F",
+			"200Rnd_556x45_Box_Red_F",
+			"200Rnd_556x45_Box_Tracer_F",
+			"200Rnd_556x45_Box_Tracer_Red_F",
+
+			"CUP_200Rnd_TE4_Red_Tracer_556x45_M249",
+			"CUP_200Rnd_TE4_Yellow_Tracer_556x45_M249",
+			"CUP_200Rnd_TE4_Green_Tracer_556x45_M249",
+			"CUP_200Rnd_TE1_Red_Tracer_556x45_M249",
+			"CUP_200Rnd_TE4_Green_Tracer_556x45_M249_Pouch",
+			"CUP_200Rnd_TE4_Red_Tracer_556x45_M249_Pouch",
+			"CUP_200Rnd_TE4_Yellow_Tracer_556x45_M249_Pouch",
+			"CUP_200Rnd_TE1_Red_Tracer_556x45_M249_Pouch",
+			"CUP_100Rnd_TE4_Green_Tracer_556x45_M249",
+			"CUP_100Rnd_TE4_Red_Tracer_556x45_M249",
+			"CUP_100Rnd_TE4_Yellow_Tracer_556x45_M249",
+			"CUP_200Rnd_TE4_Green_Tracer_556x45_L110A1",
+			"CUP_200Rnd_TE4_Red_Tracer_556x45_L110A1",
+			"CUP_200Rnd_TE4_Yellow_Tracer_556x45_L110A1",
+
+			"CUP_35Rnd_556x45_Galil_Mag",
+			"CUP_35Rnd_556x45_Red_Tracer_Galil_Mag",
+			"CUP_35Rnd_556x45_Green_Tracer_Galil_Mag",
+			"CUP_50Rnd_556x45_Galil_Mag",
+			"CUP_50Rnd_556x45_Red_Tracer_Galil_Mag",
+			"CUP_50Rnd_556x45_Green_Tracer_Galil_Mag"
+		};
+	};
 };
