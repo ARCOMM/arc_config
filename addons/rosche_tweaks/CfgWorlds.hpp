bfe45eedaa61dcaf81e4421af46517b2d60058ba
Drofseh <Drofseh@users.noreply.github.com>
2024-11-11 11:00:30 -0800

    Create rosche_tweaks addon (#10)
    - fixes map grid not matching GPS coords

---
+++ addons/rosche_tweaks/CfgWorlds.hpp
@@ -0,0 +1,14 @@
+class CfgWorlds {
+    class DefaultWorld;
+
+    class CAWorld : DefaultWorld {
+        class Grid;
+    };
+
+    class WL_Rosche : CAWorld {
+        class Grid : Grid {
+            offsetX = 0;
+            offsetY = 15360;
+        };
+    };
+} ;
