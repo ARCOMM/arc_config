f580aa7b453087dbd771b7315cfeaec9bcfe80fc
Drofseh <Drofseh@users.noreply.github.com>
2025-04-09 00:11:55 -0700

    Create NIArms Tweaks

---
+++ addons/niarms_tweaks/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT niarms_tweaks
+#define COMPONENT_BEAUTIFIED NIArms Tweaks
+
+#include "\z\arc_cfg\addons\main\script_mod.hpp"
+#include "\z\arc_cfg\addons\main\script_macros.hpp"
+
