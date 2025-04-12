d57c3becd5e775a608217486e45d8180ece00d74
Drofseh <Drofseh@users.noreply.github.com>
2025-04-12 10:38:49 -0700

    create Aliabad Tweaks addon
    - fix offset terrain grid

---
+++ addons/aliabad_tweaks/CfgWorlds.hpp
@@ -0,0 +1,14 @@
+class CfgWorlds {
+    class DefaultWorld;
+
+    class CAWorld : DefaultWorld {
+        class Grid;
+    };
+
+    class MCN_Aliabad : CAWorld {
+        class Grid : Grid {
+            offsetX = 0;
+            offsetY = 5120;
+        };
+    };
+} ;
