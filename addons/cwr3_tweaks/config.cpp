47e6007d4900ea717671349da5b3690750e6894f
tim4242 <puzzleddev@gmail.com>
2025-04-21 16:03:16 +0200

    Remove CWR3 vest definitions

--- addons/cwr3_tweaks/config.cpp
+++ addons/cwr3_tweaks/config.cpp
@@ -4,17 +4,6 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {};
-        weapons[] = {
-            "cwr3_b_vest_pasgt_oliveDrab",
-            "cwr3_b_vest_pasgt_alice_oliveDrab",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_etool",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_ar",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_gl",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_mg",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_officer",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_light",
-            "cwr3_b_vest_pasgt_alice_oliveDrab_medic"
-        };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "arc_cfg_main",
