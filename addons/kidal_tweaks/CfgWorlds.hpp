343d3cbf93b67053d888fddb1c51223554c2454b
Drofseh <Drofseh@users.noreply.github.com>
2025-05-15 17:17:11 -0700

    adjust grid zoom levels to make it less cluttered

--- addons/kidal_tweaks/CfgWorlds.hpp
+++ addons/kidal_tweaks/CfgWorlds.hpp
@@ -9,6 +9,13 @@ class CfgWorlds {
         class Grid : Grid {
             offsetX = 0;
             offsetY = 61440;
+
+            class Zoom1 { // 100 m grid squares
+                zoomMax = 0.1; // default was 0.2
+            };
+            class Zoom2 { // 1000 m grid squares
+                zoomMax = 0.5; // default was 0.95
+            };
         };
     };
 } ;
