cbf73629b62ae7e87147d20ee02cd8b2d42e03ea
Drofseh <Drofseh@users.noreply.github.com>
2025-06-28 15:42:02 -0700

    Improve XM250 ACE heat capacity

--- addons/kar_tweaks/CfgWeapons.hpp
+++ addons/kar_tweaks/CfgWeapons.hpp
@@ -2,6 +2,7 @@ class CfgWeapons {
     class Rifle_Base_F;
     class KAR_XM250 : Rifle_Base_F {
         ace_overheating_allowSwapBarrel = 1;
+        ace_overheating_barrelMass = 900;
         ace_overheating_closedBolt = 0;
     };
 };
