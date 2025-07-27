1fd0d34a5071d9f757e65104c865697951d65efa
Drofseh <Drofseh@users.noreply.github.com>
2025-07-27 13:04:59 -0700

    ace_tweaks - adjust painkiller effectiveness (#29)
    * Create ace_medical_treatment.hpp
    
    * Update config.cpp
    
    * Update ace_medical_treatment.hpp

--- addons/ace_tweaks/config.cpp
+++ addons/ace_tweaks/config.cpp
@@ -20,5 +20,6 @@ class CfgPatches {
 
 #include "ACE_detector.hpp"
 #include "ace_medical_replacementItems.hpp"
+#include "ace_medical_treatment.hpp"
 #include "Cfg3DEN.hpp"
 #include "CfgEventHandlers.hpp"
